021b6177786b8a7ce7524dfdb8f98b199e7c187b
astrokachi <astrokachi@gmail.com>
2022-08-09 17:38:47 +0100

    finsihed cart page

--- src/LoginTab.js
+++ src/LoginTab.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './LoginTab.css'
+import { Link } from "react-router-dom";
 
 const LoginTab = () => {
   return (
     <div className='loginTab'>
         <h1>Login</h1>
+        <p>Enter your email and password</p>
         <form className='loginTab_form' action="">
             <input type="Email" placeholder='Email' />
             <input type="Password" placeholder='Password' />
-            <p><a href="/">Forget your password ?</a></p>
             <button>SIGN IN</button>
-            <p><a href="/">Create account</a></p>
+            <p><Link to="/">Forget your password ?</Link></p>
+            <p><Link to="/signup">Create account</Link></p>
         </form>
-        <div className='loginTab_lower_div'>
+        {/* <div className='loginTab_lower_div'>
             <p><a href="/">Return to Store</a></p>
             <p className='login_or'>or sign in with</p>
             <div className='loginTab_signin_options'>
@@ -22,7 +24,7 @@ const LoginTab = () => {
                 <button>LinkedIn</button>
                 <button>Amazon</button>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
